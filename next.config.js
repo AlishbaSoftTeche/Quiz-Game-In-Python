c3f632eb1f555575d59e69fe0049fb22fd434e7f
alishba002 <alishbaabbas008@gmail.com>
2023-08-03 10:12:21 +0500

    Remove 'NFT-Market-place-FYP-/' directory

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["maryam-nft-marketplace.infura-ipfs.io", "infura-ipfs.io"],
+    // https://maryam-nft-marketplace.infura-ipfs.io
+    //daulat-nft-marketplace.infura-ipfs.io
+    formats: ["image/webp"],
+  },
+};
