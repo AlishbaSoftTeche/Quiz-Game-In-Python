f945384f8cb49cacfffcbc7f19248f5d2d8011b2
alishba002 <alishbaabbas008@gmail.com>
2023-08-08 16:01:21 +0500

    deployment

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["maryam-nft-marketplace.infura-ipfs.io", "infura-ipfs.io"],
-    // https://maryam-nft-marketplace.infura-ipfs.io
-    //daulat-nft-marketplace.infura-ipfs.io
-    formats: ["image/webp"],
-  },
-};
